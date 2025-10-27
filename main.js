5b976c1a1fbcef4933fafe905477a4a486b7a666
Stas bc2025-4 <malinovskyistas@gmail.com>
2025-10-27 19:39:52 +0200

    Add basic HTTP server logic

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// JavaScript source code
+import { Command } from "commander";
+import http from "http";
+import fs from "fs/promises";
+
+const program = new Command();
+
+program
+    .requiredOption("-i, --input <path>", "шлях до вхідного файлу JSON")
+    .requiredOption("-h, --host <host>", "адреса сервера")
+    .requiredOption("-p, --port <port>", "порт сервера");
+
+program.parse();
+const options = program.opts();
+
+http.createServer((req, res) => {
+    res.end("Server works!");
+}).listen(options.port, options.host, () => {
+    console.log("Server started");
+});
